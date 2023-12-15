8943b7b84edab60225b4aadf8fe00fb5c10e3704
mlewandowski016 <mateuszlewandowski016@gmail.com>
2023-12-15 22:08:13 +0100

    vin

---
+++ AudiVin.cs
@@ -0,0 +1,14 @@
+﻿namespace Basics
+{
+    internal class AudiVin : Vin
+    {
+
+        public AudiVin(string vin) : base(vin)
+        {
+            if(!vin.StartsWith("WAUZZZ"))
+            {
+                throw new ArgumentException("Niepoprawny VIN dla Audi.");
+            }
+        }
+    }
+}
